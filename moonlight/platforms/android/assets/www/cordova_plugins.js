283375ad6a2fe84660af7eae0cb8e71c5af78e4b
JesseScott <jessecolinscott@gmail.com>
2015-02-08 23:23:02 -0800

    added plugins and built

--- moonlight/platforms/android/assets/www/cordova_plugins.js
+++ moonlight/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,42 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.console": "0.2.12",
+    "org.apache.cordova.device": "0.2.13",
+    "org.apache.cordova.splashscreen": "0.3.5",
+    "org.apache.cordova.dialogs": "0.2.11",
+    "org.apache.cordova.geolocation": "0.3.11"
+}
 // BOTTOM OF METADATA
 });
