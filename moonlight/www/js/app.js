2514a80fb0204921d790ce3ad095f7e000f15a87
JesseScott <jessecolinscott@gmail.com>
2015-05-18 22:56:20 -0700

    got ngRoute loading finally

--- moonlight/www/js/app.js
+++ moonlight/www/js/app.js
@@ -1,24 +1,24 @@
+'use strict';
 
-var moonlight = angular.module('moonlight', [
-	'ngRoute',
-	'moonlightControllers'
-]);
 
-moonlight.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  when('/light', {
-    templateUrl: 'partials/light.html',
-    controller: 'LightController'
-  }).
-  when('/data', {
-    templateUrl: 'partials/data.html',
-    controller: 'DataController'
-  }).
-  when('/about', {
-    templateUrl: 'partials/about.html',
-    controller: 'AboutController'
-  }).
-  otherwise({
-    redirectTo: '/light'
-  });
-}]);
+	var moonlight = angular.module('moonlight', ['ngRoute','moonlight.controllers']);
+	//var moonlight = angular.module('moonlight', ['ngRoute']);
+
+	moonlight.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider.
+	  when('/light', {
+	    templateUrl: 'partials/light.html',
+	    controller: 'LightController'
+	  }).
+	  when('/data', {
+	    templateUrl: 'partials/data.html',
+	    controller: 'DataController'
+	  }).
+	  when('/about', {
+	    templateUrl: 'partials/about.html',
+	    controller: 'AboutController'
+	  }).
+	  otherwise({
+	    redirectTo: '/light'
+	  });
+	}]);
