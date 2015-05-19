e5aee28e74fb2fc9127036aed462f6ea2cbc1cc4
JesseScott <jessecolinscott@gmail.com>
2015-05-18 23:44:06 -0700

    swiping to next route

--- moonlight/www/js/controllers.js
+++ moonlight/www/js/controllers.js
@@ -2,12 +2,20 @@
 angular.module('moonlight.controllers', []);
 
 angular.module('moonlight.controllers')
-.controller('LightController', ['$scope', '$http', function($scope, $http) {
+.controller('LightController', function($scope, $http, $location) {
 
-}])
+  $scope.changeView = function(view) {
+    console.log('CHANGING VIEW TO ' + view);
+    $location.url(view);
+  }
+
+})
 .controller('DataController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
+
 
 }])
 .controller('AboutController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
+
+
 
 }]);
