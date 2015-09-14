1a73286fa3f0810081f2400d6c6781715eed5106
JesseScott <jessecolinscott@gmail.com>
2015-09-13 20:51:16 -0700

    bad controller

--- moonlight/www/js/controllers.js
+++ moonlight/www/js/controllers.js
@@ -2,27 +2,27 @@
 angular.module('moonlight.controllers', []);
 
 angular.module('moonlight.controllers')
-.controller('LightController', function($scope, $http, $location) {
+.controller('LightController', function($scope, $http, $location, $routeParams) {
 
   $scope.changeView = function(view) {
-    console.log('CHANGING VIEW TO ' + view);
+    console.log('/light --> ' + view);
     $location.url(view);
   }
 
 })
-.controller('DataController', function($scope, $http, $routeParams) {
+.controller('DataController', function($scope, $http, $location, $routeParams) {
 
   $scope.changeView = function(view) {
-    console.log('CHANGING VIEW TO ' + view);
+    console.log('/data --> ' + view);
     $location.url(view);
   }
 
 })
-.controller('AboutController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
+.controller('AboutController', function($scope, $http, $location, $routeParams) {
 
   $scope.changeView = function(view) {
-    console.log('CHANGING VIEW TO ' + view);
+    console.log('about --> ' + view);
     $location.url(view);
   }
 
-}]);
+});
