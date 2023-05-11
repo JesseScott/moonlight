ea80ce3b4e892f1521756da73ca2de4f1cd358f1
JesseScott <jessecolinscott@gmail.com>
2023-05-10 21:06:59 -0700

    Remove moonlight_old

--- moonlight_old/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/jessescott/Documents/Development/_CODE_/GIT/moonlight/moonlight/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/jessescott/Documents/Development/_CODE_/GIT/moonlight/moonlight/platforms/android/CordovaLib/AndroidManifest.xml \
--- moonlight_old/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/jessescott/Documents/Development/_CODE_/GIT/moonlight/moonlight/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/jessescott/Documents/Development/_CODE_/GIT/moonlight/moonlight/platforms/android/AndroidManifest.xml \
