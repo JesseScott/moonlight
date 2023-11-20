39d03537e42d75f7262584c0d22b66b1f5b62833
JesseScott <jessecolinscott@gmail.com>
2023-11-19 22:53:36 -0800

    Bit more cleanup

--- app/androidApp/src/main/java/tt/co/jesses/moonlight/android/view/MoonlightScreen.kt
+++ app/androidApp/src/main/java/tt/co/jesses/moonlight/android/view/MoonlightScreen.kt
@@ -1,34 +1,24 @@
 package tt.co.jesses.moonlight.android.view
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun MoonlightScreen(
     viewModel: MoonlightViewModel = viewModel(),
 ) {
-    val uiState = viewModel.uiState.collectAsState()
-    val fraction = uiState.value.illuminationData.fraction
-    val padding = 16.dp
-    val normalizedPhase = ((uiState.value.illuminationData.phase + 180f) / 360f).coerceIn(0.0, 1.0)
+    val illuminationData = viewModel.uiState.collectAsState().value.illuminationData
+    val normalizedPhase = ((illuminationData.phase + 180f) / 360f).coerceIn(0.0, 1.0)
 
     Canvas(
         modifier = Modifier
@@ -45,7 +35,7 @@ fun MoonlightScreen(
             ),
             topLeft = Offset.Zero,
             size = size,
-            alpha = fraction.toFloat(),
+            alpha = illuminationData.fraction.toFloat(),
             colorFilter = ColorFilter.lighting(
                 multiply = Color.White,
                 add = Color.Yellow,
