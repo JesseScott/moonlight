e97dae0905d447b17b0973c343630d583217fd93
JesseScott <jessecolinscott@gmail.com>
2024-07-17 22:35:16 -0700

    Upgrade to SDK 34 and Java 17

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 android {
     namespace = "tt.co.jesses.moonlight.android"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "tt.co.jesses.moonlight.android"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "0.1"
     }
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
