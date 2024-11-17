8b93c090dc06ba363716a8ee38f9e20c37867aae
JesseScott <jessecolinscott@gmail.com>
2024-11-16 16:08:58 -0800

    versionCode 3 versionName 0.3

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "tt.co.jesses.moonlight.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
     buildFeatures {
         compose = true
