2899dfb9f75fb901ae3fc84fdd9952ac38d865b6
JesseScott <jessecolinscott@gmail.com>
2024-12-16 17:50:25 -0800

    Update Compose + AndroidX dependency versions

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -50,14 +50,14 @@ android {
 dependencies {
     implementation(project(":shared"))
 
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.activity:activity-compose:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.foundation:foundation:1.7.6")
+    implementation("androidx.compose.material:material:1.7.6")
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("com.google.android.gms:play-services-oss-licenses:17.1.0")
 
     // Hilt
